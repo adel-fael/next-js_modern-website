b3b46d5ceb7958f89b3b105df9d7a9f74da5cab4
Adel Fael <adel.fael@econcepts-me.com>
2022-12-05 19:17:31 +0200

    making accordion Anim better

--- components/Accordion.js
+++ components/Accordion.js
@@ -1,4 +1,4 @@
-import { motion } from 'framer-motion'
+import { AnimatePresence, motion } from 'framer-motion'
 import { useState } from 'react'
 import { fadeInRight } from '../variants'
 // import icons
@@ -10,53 +10,62 @@ const Accordion = ({ accordion }) => {
   // destructure accordion data
   const { question, answer } = accordion
   return (
-    <motion.div variants={fadeInRight} className="max-w-[550px]">
-      <button
-        onClick={() => {
-          setIsOpen(!isOpen)
-        }}
-        className="drop-shadow-primary bg-white rounded-[10px] h-[90px] px-[35px] flex items-center text-left w-[100%]"
-      >
-        <div className="w-full flex justify-between items-center">
-          {/* title  */}
-          <p className="lead font-medium leading-snug max-w-[400px]">
-            {question}
-          </p>
-          {/* icons  */}
-          <div className="transition-all duration-500">
-            {isOpen ? (
-              <motion.div
-                initial="initial"
-                animate={{
-                  rotate: 180,
-                }}
-              >
-                <HiOutlineMinus className="text-[28px] text-accent" />
-              </motion.div>
-            ) : (
-              <motion.div
-                initial="initial"
-                animate={{
-                  rotate: 0,
-                }}
-              >
-                <HiOutlinePlus className="text-[28px] text-accent" />
-              </motion.div>
-            )}
+    <AnimatePresence
+            
+            initial={false}
+ 
+            exitBeforeEnter={true}
+          
+            onExitComplete={() => null}
+    >
+      <motion.div variants={fadeInRight} className="max-w-[550px]">
+        <button
+          onClick={() => {
+            setIsOpen(!isOpen)
+          }}
+          className="drop-shadow-primary bg-white rounded-[10px] h-[90px] px-[35px] flex items-center text-left w-[100%]"
+        >
+          <div className="w-full flex justify-between items-center">
+            {/* title  */}
+            <p className="lead font-medium leading-snug max-w-[400px]">
+              {question}
+            </p>
+            {/* icons  */}
+            <div className="transition-all duration-500">
+              {isOpen ? (
+                <motion.div
+                  initial="initial"
+                  animate={{
+                    rotate: 180,
+                  }}
+                >
+                  <HiOutlineMinus className="text-[28px] text-accent" />
+                </motion.div>
+              ) : (
+                <motion.div
+                  initial="initial"
+                  animate={{
+                    rotate: 0,
+                  }}
+                >
+                  <HiOutlinePlus className="text-[28px] text-accent" />
+                </motion.div>
+              )}
+            </div>
           </div>
+        </button>
+        {/* answer  */}
+        <div
+          className={`${
+            isOpen
+              ? 'max-h-[160px] p-8 bg-[#fff7f5] rounded-[10px] drop-shadow-primary my-2'
+              : 'max-h-0'
+          } h-[160px] overflow-hidden transition-all`}
+        >
+          <p className="lead leading-[30px] mt-2">{answer}</p>
         </div>
-      </button>
-      {/* answer  */}
-      <div
-        className={`${
-          isOpen
-            ? 'max-h-[160px] p-8 bg-[#fff7f5] rounded-[10px] drop-shadow-primary my-2'
-            : 'max-h-0'
-        } h-[160px] overflow-hidden transition-all`}
-      >
-        <p className="lead leading-[30px] mt-2">{answer}</p>
-      </div>
-    </motion.div>
+      </motion.div>
+    </AnimatePresence>
   )
 }
 
